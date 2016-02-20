6d27e1e6cf0f7b0c4b85f3b565921c41f9e41ac1
hisaitami <hisaitami@gmail.com>
2016-02-20 21:10:47 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# An example for myThings and Belize on Intel Edison
+
---
+++ main.js
@@ -0,0 +1,12 @@
+var five = require('johnny-five');
+var Edison = require('edison-io');
+var board = new five.Board({
+  io: new Edison()
+});
+
+board.on('ready', function() {
+  var led = new five.Led(13);
+  led.blink();
+
+  board.servoWrite(3, 3);
+});
